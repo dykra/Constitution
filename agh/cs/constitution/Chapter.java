3c01f961a69285acc4dc435a0b846245c02d6b41
dykra <asiapalewicz@gmail.com>
2016-12-02 17:30:51 +0100

    chapter articleMin/Max added

--- agh/cs/constitution/Chapter.java
+++ agh/cs/constitution/Chapter.java
@@ -9,6 +9,8 @@ public class Chapter implements IConstitutionElement {
     public ArrayList<IConstitutionElement> articles;
     private int number; //korzysta z metody roman to decimal, ale czy ona na pewno potrzebna? lepiej nr po kolei dodawani
     private String title;
+    int articleMin;
+    int articleMax;
 
     @Override
     public void toString(int number) {
